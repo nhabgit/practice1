7a349e56b61aafa358a0daa2c7076d37b0d87287
Noel Habito <nhabito@noel-habitos-macbook.local>
2016-05-04 16:30:57 -0500

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//This is one line comment.
+		/*This is a multi-line
+		 * comment.
+		 */
+		System.out.println("Hello World!");
+		
+	}
+
+}
